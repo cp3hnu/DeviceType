0c3626da01c4aa101e110c631089f26497df6431
WeiZhao <wei.zhao-1@datayp.com>
2017-08-16 10:14:49 +0800

    Swift 3.0

--- DeviceType/AppDelegate.swift
+++ DeviceType/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         testDevice();
@@ -22,25 +22,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    func applicationWillResignActive(application: UIApplication) {
+    func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
-    func applicationDidEnterBackground(application: UIApplication) {
+    func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
-    func applicationWillEnterForeground(application: UIApplication) {
+    func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
-    func applicationDidBecomeActive(application: UIApplication) {
+    func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
-    func applicationWillTerminate(application: UIApplication) {
+    func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 }
@@ -49,13 +49,13 @@ extension AppDelegate {
     //Test Function
     func testDevice()
     {
-        let device = UIDevice.currentDevice()
+        let device = UIDevice.current
         
         //version
-        if device.isGE(version: "8.0") {
+        if device.isGE("8.0") {
             print("version >= 8.0")
         }
-        else if device.isGE(version: "7.0") {
+        else if device.isGE("7.0") {
             print("version == 7.0")
         }
         else {
@@ -63,17 +63,16 @@ extension AppDelegate {
         }
         
         //iPad or iPhone
-        if device.isPad() {
+        if device.isPad {
             print("iPad")
             
-            if device.isPadMini() {
+            if device.isPadMini {
                 print("iPad mini")
-            }
-            else if device.isBigPad() {
-                print("9.7-inch iPad")
+            } else {
+                print("other iPad")
             }
         }
-        else if (device.isPhone()) {
+        else if (device.isPhone) {
             print("iPhone")
         }
         
@@ -81,21 +80,21 @@ extension AppDelegate {
         let type = device.deviceType
         
         switch type {
-        case DeviceType.DT_iPhone4:
+        case DeviceType.dt_iPhone4:
             print("iPhone4S")
-        case DeviceType.DT_iPhone5:
+        case DeviceType.dt_iPhone5:
             print("iPhone5")
-        case DeviceType.DT_iPhone6:
+        case DeviceType.dt_iPhone6:
             print("iPhone6")
-        case DeviceType.DT_iPhone6_Plus:
+        case DeviceType.dt_iPhone6_Plus:
             print("iPhone6_Plus")
-        case DeviceType.DT_iPad:
+        case DeviceType.dt_iPad:
             print("iPad")
-        case DeviceType.DT_iPad_Mini:
+        case DeviceType.dt_iPad_Mini:
             print("iPad_Mini")
-        case DeviceType.DT_iPad_Retina:
+        case DeviceType.dt_iPad_Retina:
             print("iPad_Retina")
-        case DeviceType.DT_iPad_Mini_Retina:
+        case DeviceType.dt_iPad_Mini_Retina:
             print("iPad_Mini_Retina")
         default:
             print("Unknown")
---
+++ DeviceType/DeviceType/UIDevice.swift
@@ -0,0 +1,127 @@
+//
+//  Device.swift
+//  DeviceType
+//
+//  Created by ZhaoWei on 14-9-28.
+//  Copyright (c) 2014年 csdept. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public enum DeviceType: Int
+{
+    case dt_unknown = 0
+    case dt_iPhone4             //iPhone4、iPhone4S
+    case dt_iPhone5             //iPhone5、iPhone5C和iPhone5S
+    case dt_iPhone6             //iPhone6、iPhone6S、iPhone7
+    case dt_iPhone6_Plus        //iPhone6 Plus、iPhone6S Plus、iPhone7 Plus
+    case dt_iPad                //iPad1、iPad2
+    case dt_iPad_Mini           //iPad mini1
+    case dt_iPad_Retina         //New iPad、iPad4和iPad Air
+    case dt_iPad_Mini_Retina    //iPad mini2
+    case dt_iPad_Pro_12_9_Inch  //iPad Pro 12.9
+    case dt_iPad_Pro_10_5_Inch  //iPad Pro 10.5
+}
+
+extension UIDevice {
+    /**
+     根据屏幕分辨率判断设备类型
+     */
+    public var deviceType: DeviceType {
+        guard let size = UIScreen.main.currentMode?.size else {
+            return .dt_unknown
+        }
+        
+        switch size {
+        case CGSize(width: 640 , height: 960 ): return .dt_iPhone4
+        case CGSize(width: 640 , height: 1136): return .dt_iPhone5
+        case CGSize(width: 750 , height: 1334): return .dt_iPhone6
+        case CGSize(width: 1242, height: 2208): return .dt_iPhone6_Plus
+        case CGSize(width: 1024, height: 768 ): return .dt_iPad
+        case CGSize(width: 768 , height: 1024): return .dt_iPad_Mini
+        case CGSize(width: 2048, height: 1536): return .dt_iPad_Retina
+        case CGSize(width: 1536, height: 2048): return .dt_iPad_Mini_Retina
+        case CGSize(width: 2732, height: 2048): return .dt_iPad_Pro_12_9_Inch
+        case CGSize(width: 2224, height: 1668): return .dt_iPad_Pro_10_5_Inch
+        default: return .dt_unknown
+        }
+    }
+    
+    /**
+     判断当前设备是不是iPhone设备
+     */
+    public var isPhone: Bool {
+        return UIDevice.current.userInterfaceIdiom == .phone
+    }
+    
+    /**
+     判断当前设备是不是iPad设备
+     */
+    public var isPad: Bool {
+        return UIDevice.current.userInterfaceIdiom == .pad
+    }
+    
+    /**
+     判断当前设备是不是iPad mini
+     */
+    public var isPadMini: Bool {
+        return deviceType == .dt_iPad_Mini || deviceType == .dt_iPad_Mini_Retina
+    }
+    
+    /**
+     判断当前设备是不是iPhone4
+     */
+    public var isIPhone4: Bool {
+        return deviceType == .dt_iPhone4
+    }
+    
+    /**
+     判断当前设备是不是iPhone5
+     */
+    public var isIPhone5: Bool {
+        return deviceType == .dt_iPhone5
+    }
+    
+    /**
+     判断当前设备是不是iPhone6
+     */
+    public var isIPhone6: Bool {
+        return deviceType == .dt_iPhone6
+    }
+    
+    /**
+     判断当前设备是不是iPhone6 Plus
+     */
+    public var isIPhone6Plus: Bool {
+        return deviceType == .dt_iPhone6_Plus
+    }
+}
+
+extension UIDevice {
+    /**
+     判断当前设备的系统版本是否大于或者等于version
+     */
+    public func isGE(_ version: String) -> Bool {
+        return compare(version: version) != .orderedAscending
+    }
+    
+    fileprivate func compare(version: String) -> ComparisonResult {
+        return UIDevice.current.systemVersion.compare(version, options: NSString.CompareOptions.numeric)
+    }
+}
+
+extension UIDevice {
+    public var appVersion: String {
+        return Bundle.main.infoDictionary?["CFBundleShortVersionString"] as! String
+    }
+    
+    public var appName: String {
+        return Bundle.main.infoDictionary?["CFBundleDisplayName"] as! String
+    }
+    
+    public var bundleID: String {
+        return Bundle.main.infoDictionary?["CFBundleIdentifier"] as! String
+    }
+}
+
